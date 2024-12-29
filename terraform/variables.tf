40ce435e4aa754fbd2929b84f199e25427fe4db0
batzsalmassi <sean.salmassi1@gmail.com>
2024-12-29 15:57:55 +0200

    change the desired_nodes_num

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -52,7 +52,7 @@ variable "eks_node_instance_type" {
 variable "eks_node_desired_size" {
   description = "The desired number of nodes in the EKS node group"
   type        = number
-  default = 3
+  default = 2
 }
 
 variable "eks_node_max_size" {
